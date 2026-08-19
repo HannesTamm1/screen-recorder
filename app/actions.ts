5d1a2aa165241da6d5bc4d4eb5c98ec7b3856ce8
Hannes <hannes.tamm@tptlive.ee>
2026-08-19 08:00:23 +0300

    Feat: Add Mux upload functionality

---
+++ app/actions.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import Mux from '@mux/mux-node';
+
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
+
+export async function createUploadUrl() {
+  const upload = await mux.video.uploads.create({
+    new_asset_settings: {
+      playback_policy: ['public'],
+      video_quality: 'plus',
+      mp4_support: 'standard',
+      input: [
+        {
+            generated_subtitles: [
+                { language_code: 'en', name: 'English (Auto)' }
+            ]
+        }
+      ]
+    },
+    cors_origin: '*',
+  });
+
+  return upload;
+}
